e6e8b625efb20ee1c4bf6cb66df359ac14906ee4
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-14 23:28:42 +0530

    Sync leetcode solution: Valid Parentheses [Code]

--- leetcode/Valid-Parentheses/solution.py
+++ leetcode/Valid-Parentheses/solution.py
@@ -1,1 +1,13 @@
-// Solution code not captured by API sync.
+class Solution:
+    def isValid(self, s: str) -> bool:
+        par = {")": "(", "}": "{", "]": "["}
+        stack = []
+        for c in s:
+            if c in par:
+                if stack and stack[-1] == par[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return True if not stack else False
