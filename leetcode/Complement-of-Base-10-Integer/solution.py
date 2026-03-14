67bd278825bd2cf9116516f2467ddaa8c67682ea
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-14 22:55:10 +0530

    Sync leetcode solution: Complement of Base 10 Integer [Code]

--- leetcode/Complement-of-Base-10-Integer/solution.py
+++ leetcode/Complement-of-Base-10-Integer/solution.py
@@ -1,10 +1,1 @@
-class Solution:
-    def bitwiseComplement(self, n: int) -> int:
-        if n == 0:
-            return 1
-        
-        mask = 1
-        while mask <= n:
-            mask <<= 1
-        
-        return (mask - 1) ^ n
+// Solution code captured in real-time by extension.
