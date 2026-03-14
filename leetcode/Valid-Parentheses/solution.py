4cdd8961f6679fc7b3407d30431af6817df58e03
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-15 02:06:44 +0530

    Sync leetcode solution: Valid Parentheses [Code]

--- leetcode/Valid-Parentheses/solution.py
+++ leetcode/Valid-Parentheses/solution.py
@@ -1,13 +1,10 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        par = {")": "(", "}": "{", "]": "["}
+        map = {")": "(", "}": "{", "]": "["}
         stack = []
-        for c in s:
-            if c in par:
-                if stack and stack[-1] == par[c]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(c)
-        return True if not stack else False
+        for char in s:
+            if char in map:
+                top_element = stack.pop() if stack else '#'
+                if map[char] != top_element: return False
+            else: stack.append(char)
+        return not stack
