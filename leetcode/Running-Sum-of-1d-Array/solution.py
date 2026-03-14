9cc435264f831cbaf32825b63976ba425ab53e8e
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-14 21:58:03 +0530

    Sync leetcode solution: Running Sum of 1d Array [Code]

--- leetcode/Running-Sum-of-1d-Array/solution.py
+++ leetcode/Running-Sum-of-1d-Array/solution.py
@@ -1,1 +1,5 @@
-// Solution code not captured by API sync.
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] = nums[i] + nums[i - 1]
+        return nums
