db6eab6bef0e10f7bccf98620994aba1948d3420
Aditya-Raj-25 <adityarajsrivastava227@gmail.com>
2026-03-14 21:52:20 +0530

    Sync leetcode solution: Subsets [Code]

--- leetcode/Subsets/solution.py
+++ leetcode/Subsets/solution.py
@@ -1,1 +1,11 @@
-// Solution code not captured by API sync.
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = [[]] 
+        for num in nums:
+            newsubsets = []
+            for curr in result:
+                temp = curr.copy()
+                temp.append(num)
+                newsubsets.append(temp)
+            result.extend(newsubsets) 
+        return result
